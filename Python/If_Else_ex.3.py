0b9d110b8ddbc7bb0193d53bed99784114503b88
BiaS3 <157229060+BiaS3@users.noreply.github.com>
2026-06-10 13:47:04 +0100

    Create If_Else_ex.3.py

---
+++ Python/If_Else_ex.3.py
@@ -0,0 +1,14 @@
+#Contrua um programa que calcule:
+#Qual deve ser o bônus de um funcionário
+#Se ele vendeu mais de 1000 unidades, o bônus é de 250, caso contrário, o bonus é de 50
+
+Vendas = 1200
+Meta = 1000
+
+if Vendas > Meta:
+    bonus = 250
+else:
+    bonus = 50
+
+print(bonus)
+
