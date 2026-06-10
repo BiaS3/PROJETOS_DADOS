7ffb623a6cbcf355ca76041f09cd95a5379f8132
BiaS3 <157229060+BiaS3@users.noreply.github.com>
2026-06-10 13:16:48 +0100

    Create Estudando_Algoritmo_ex1.py

---
+++ Python/Estudando_Algoritmo_ex1.py
@@ -0,0 +1,10 @@
+# Construa um Programa que calcule: Quanto devemos cobrar em um projeto de pro...
+# se trabalhamos 8h por dia, um total de 15 dias, e cobramos R$100/hora.?
+
+horas_por_dia = 8
+dias_totais = 15
+custo_hora = 100
+
+valor_cobrar = horas_por_dia * dias_totais * custo_hora
+
+print(valor_cobrar)
