cd10c4197e57fd596c68d27cb20e05b67c47b8b5
BiaS3 <157229060+BiaS3@users.noreply.github.com>
2026-06-10 13:31:39 +0100

    Create Estudando_Algoritmo_ex2.py

---
+++ Python/Estudando_Algoritmo_ex2.py
@@ -0,0 +1,12 @@
+#Construa um programa que calcule:
+#Quanto custa encher o tanque de um carro que tem 50 litros de capacidade, está com com 20 litros de combustível atualmente e o custo do combustível é de €5,80/litro?
+
+Capacidade = 50
+Tanque = 20
+Valor = 5.80
+
+#Para abastecer o tanque vamos efetuar o calculo de quantos litros faltam para completar * o valor por litro.
+
+Custo = (Capacidade - Tanque) * Valor
+
+print(Custo)
