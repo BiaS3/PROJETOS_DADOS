f5550ad40aa6deaed2175f2bf351fdd67ab8a736
BiaS3 <157229060+BiaS3@users.noreply.github.com>
2026-06-11 13:19:12 +0100

    Create if_else_and.ex4.py

---
+++ Python/if_else_and.ex4.py
@@ -0,0 +1,13 @@
+# Qual deve ser o bônus do funcionário?
+# Se a empresa tem a meta de vendas de 10.000 e o funcionário meta de 1.000
+# Bonus = 250 , Se não 50
+
+Vendas = 1200
+meta = 1000
+
+if Vendas > 1000 and meta == 1000:
+    bonus = 250
+else:
+    bonus = 50
+
+print(bonus)
